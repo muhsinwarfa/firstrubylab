77581d8555a8d1c886129e7bccb2d4aeb1bf9672
Muhsin <muhsin.warfa@gmail.com>
2016-09-08 15:51:11 +0300

    First ruby Lab assignment

---
+++ regex_tester.rb
@@ -0,0 +1,18 @@
+class RegexTester
+	def initialize(pattern = nil )
+		@pattern = pattern unless pattern.nil?
+	end
+
+	attr_accessor :pattern
+
+	def statement=(statement)
+		@statement = statement
+	end
+
+	def statement
+		@statement
+	end
+
+
+
+end
